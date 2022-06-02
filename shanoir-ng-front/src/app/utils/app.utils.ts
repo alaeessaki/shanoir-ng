60e429d3833208c981b45969a0780bdf8c8a740a
Alae Es-saki <alaeessaki@gmail.com>
2022-06-02 13:36:52 +0200

    changing default dataset file type

--- shanoir-ng-front/src/app/utils/app.utils.ts
+++ shanoir-ng-front/src/app/utils/app.utils.ts
@@ -125,7 +125,7 @@ export const BACKEND_API_PRECLINICAL_MS_URL: string = BACKEND_API_URL + '/precli
 
 // carmin
 export const CARMIN_BASE_URL : string = BACKEND_API_URL + "/vip/rest";
-export const BACKEND_API_CARMIN_DATASET_PROCESSING_URL : string = BACKEND_API_DATASET_MS_URL + '/carminDatasetProcessing';
+export const BACKEND_API_CARMIN_DATASET_PROCESSING_URL: string = BACKEND_API_DATASET_MS_URL + '/carminDatasetProcessing';
 
 export function hasUniqueError(error: any, fieldName: string): boolean {
     let hasUniqueError = false;
