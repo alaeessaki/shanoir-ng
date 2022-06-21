fd5ad147073229a5676a53e9b56c66743a9b85a3
KhalilKes <akakhalilyo@gmail.com>
2022-06-21 12:26:49 +0100

    adding list of carmin processings endpoint

--- shanoir-ng-datasets/src/main/java/org/shanoir/ng/processing/carmin/controller/CarminDatasetProcessingApi.java
+++ shanoir-ng-datasets/src/main/java/org/shanoir/ng/processing/carmin/controller/CarminDatasetProcessingApi.java
@@ -31,45 +31,55 @@ import io.swagger.annotations.ApiResponses;
 @RequestMapping("/carminDatasetProcessing")
 public interface CarminDatasetProcessingApi {
 
-    @ApiOperation(value = "", notes = "Saves a new carmin dataset processing", response = CarminDatasetProcessingDTO.class, tags = {})
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "created dataset processing", response = CarminDatasetProcessingDTO.class),
-            @ApiResponse(code = 401, message = "unauthorized", response = Void.class),
-            @ApiResponse(code = 403, message = "forbidden", response = Void.class),
-            @ApiResponse(code = 422, message = "bad parameters", response = ErrorModel.class),
-            @ApiResponse(code = 500, message = "unexpected error", response = ErrorModel.class) })
-    @PostMapping(value = "", produces = { "application/json" }, consumes = {
-            "application/json" })
-    @PreAuthorize("hasAnyRole('ADMIN', 'EXPERT')")
-    ResponseEntity<CarminDatasetProcessingDTO> saveNewCarminDatasetProcessing(
-            @ApiParam(value = "carmin dataset processing to create", required = true) @Valid @RequestBody CarminDatasetProcessing carminDatasetProcessing,
-            BindingResult result) throws RestServiceException;
+        @ApiOperation(value = "", notes = "Saves a new carmin dataset processing", response = CarminDatasetProcessingDTO.class, tags = {})
+        @ApiResponses(value = {
+                        @ApiResponse(code = 200, message = "created dataset processing", response = CarminDatasetProcessingDTO.class),
+                        @ApiResponse(code = 401, message = "unauthorized", response = Void.class),
+                        @ApiResponse(code = 403, message = "forbidden", response = Void.class),
+                        @ApiResponse(code = 422, message = "bad parameters", response = ErrorModel.class),
+                        @ApiResponse(code = 500, message = "unexpected error", response = ErrorModel.class) })
+        @PostMapping(value = "", produces = { "application/json" }, consumes = {
+                        "application/json" })
+        @PreAuthorize("hasAnyRole('ADMIN', 'EXPERT')")
+        ResponseEntity<CarminDatasetProcessingDTO> saveNewCarminDatasetProcessing(
+                        @ApiParam(value = "carmin dataset processing to create", required = true) @Valid @RequestBody CarminDatasetProcessing carminDatasetProcessing,
+                        BindingResult result) throws RestServiceException;
 
-    @ApiOperation(value = "", notes = "If exists, returns the dataset processing corresponding to the given id with carmin new fields", response = CarminDatasetProcessingDTO.class, tags = {})
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "found dataset processing", response = CarminDatasetProcessingDTO.class),
-            @ApiResponse(code = 401, message = "unauthorized", response = Void.class),
-            @ApiResponse(code = 403, message = "forbidden", response = Void.class),
-            @ApiResponse(code = 404, message = "no dataset processing found", response = Void.class),
-            @ApiResponse(code = 500, message = "unexpected error", response = ErrorModel.class) })
-    @GetMapping(value = "", produces = { "application/json" })
-    @PreAuthorize("hasAnyRole('ADMIN', 'EXPERT', 'USER')")
-    ResponseEntity<CarminDatasetProcessingDTO> findCarminDatasetProcessingByComment(
-            @ApiParam(value = "id of the dataset processing", required = true) @RequestParam("comment") String comment);
+        @ApiOperation(value = "", notes = "If exists, returns the carmin dataset processing corresponding to the given execution identifier with carmin new fields", response = CarminDatasetProcessingDTO.class, tags = {})
+        @ApiResponses(value = {
+                        @ApiResponse(code = 200, message = "found dataset processing", response = CarminDatasetProcessingDTO.class),
+                        @ApiResponse(code = 401, message = "unauthorized", response = Void.class),
+                        @ApiResponse(code = 403, message = "forbidden", response = Void.class),
+                        @ApiResponse(code = 404, message = "no dataset processing found", response = Void.class),
+                        @ApiResponse(code = 500, message = "unexpected error", response = ErrorModel.class) })
+        @GetMapping(value = "", produces = { "application/json" })
+        @PreAuthorize("hasAnyRole('ADMIN', 'EXPERT', 'USER')")
+        ResponseEntity<CarminDatasetProcessingDTO> findCarminDatasetProcessingByComment(
+                        @ApiParam(value = "identifier of the execution", required = true) @RequestParam("identifier") String identifier);
 
+        @ApiOperation(value = "", notes = "If exists, returns the dataset processing corresponding to the given id with carmin new fields", response = CarminDatasetProcessingDTO.class, responseContainer = "List", tags = {})
+        @ApiResponses(value = {
+                        @ApiResponse(code = 200, message = "found dataset processing", response = CarminDatasetProcessingDTO.class, responseContainer = "List"),
+                        @ApiResponse(code = 401, message = "unauthorized", response = Void.class),
+                        @ApiResponse(code = 403, message = "forbidden", response = Void.class),
+                        @ApiResponse(code = 404, message = "no dataset processing found", response = Void.class),
+                        @ApiResponse(code = 500, message = "unexpected error", response = ErrorModel.class) })
+        @GetMapping(value = "carminDatasetProcessings", produces = { "application/json" })
+        @PreAuthorize("hasAnyRole('ADMIN', 'EXPERT', 'USER')")
+        ResponseEntity<List<CarminDatasetProcessingDTO>> findCarminDatasetProcessings();
 
-    @ApiOperation(value = "", notes = "Updates a dataset processing", response = Void.class, tags = {})
-    @ApiResponses(value = { @ApiResponse(code = 204, message = "dataset processing updated", response = Void.class),
-            @ApiResponse(code = 401, message = "unauthorized", response = Void.class),
-            @ApiResponse(code = 403, message = "forbidden", response = Void.class),
-            @ApiResponse(code = 422, message = "bad parameters", response = ErrorModel.class),
-            @ApiResponse(code = 500, message = "unexpected error", response = ErrorModel.class) })
-    @PutMapping(value = "/{datasetProcessingId}", produces = { "application/json" }, consumes = {
-            "application/json" })
-    @PreAuthorize("hasAnyRole('ADMIN', 'EXPERT')")
-    ResponseEntity<Void> updateCarminDatasetProcessing(
-            @ApiParam(value = "id of the dataset processing", required = true) @PathVariable("datasetProcessingId") Long datasetProcessingId,
-            @ApiParam(value = "carmin dataset processing to update", required = true) @Valid @RequestBody CarminDatasetProcessing carminDatasetProcessing,
-            BindingResult result)
-            throws RestServiceException;
+        @ApiOperation(value = "", notes = "Updates a dataset processing", response = Void.class, tags = {})
+        @ApiResponses(value = { @ApiResponse(code = 204, message = "dataset processing updated", response = Void.class),
+                        @ApiResponse(code = 401, message = "unauthorized", response = Void.class),
+                        @ApiResponse(code = 403, message = "forbidden", response = Void.class),
+                        @ApiResponse(code = 422, message = "bad parameters", response = ErrorModel.class),
+                        @ApiResponse(code = 500, message = "unexpected error", response = ErrorModel.class) })
+        @PutMapping(value = "/{datasetProcessingId}", produces = { "application/json" }, consumes = {
+                        "application/json" })
+        @PreAuthorize("hasAnyRole('ADMIN', 'EXPERT')")
+        ResponseEntity<Void> updateCarminDatasetProcessing(
+                        @ApiParam(value = "id of the dataset processing", required = true) @PathVariable("datasetProcessingId") Long datasetProcessingId,
+                        @ApiParam(value = "carmin dataset processing to update", required = true) @Valid @RequestBody CarminDatasetProcessing carminDatasetProcessing,
+                        BindingResult result)
+                        throws RestServiceException;
 }
--- shanoir-ng-datasets/src/main/java/org/shanoir/ng/processing/carmin/service/CarminDatasetPorcessingServiceImpl.java
+++ shanoir-ng-datasets/src/main/java/org/shanoir/ng/processing/carmin/service/CarminDatasetPorcessingServiceImpl.java
@@ -1,8 +1,10 @@
 package org.shanoir.ng.processing.carmin.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.shanoir.ng.processing.carmin.model.CarminDatasetProcessing;
+import org.shanoir.ng.processing.carmin.repository.CarminDatasetProcessingRepository;
 import org.shanoir.ng.processing.model.DatasetProcessing;
 import org.shanoir.ng.processing.repository.DatasetProcessingRepository;
 import org.shanoir.ng.shared.event.ShanoirEvent;
@@ -10,67 +12,81 @@ import org.shanoir.ng.shared.event.ShanoirEventService;
 import org.shanoir.ng.shared.event.ShanoirEventType;
 import org.shanoir.ng.shared.exception.EntityNotFoundException;
 import org.shanoir.ng.utils.KeycloakUtil;
+import org.shanoir.ng.utils.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CarminDatasetPorcessingServiceImpl implements CarminDatasetProcessingService {
 
-    @Autowired
-    private DatasetProcessingRepository datasetProcessingRepository;
+        @Autowired
+        private DatasetProcessingRepository datasetProcessingRepository;
 
-    @Autowired
-    private ShanoirEventService eventService;
+        @Autowired
+        private CarminDatasetProcessingRepository carminDatasetProcessingRepository;
 
-    protected CarminDatasetProcessing updateValues(CarminDatasetProcessing from, CarminDatasetProcessing to) {
-        to.setIdentifier(from.getIdentifier());
-        to.setStatus(from.getStatus());
-        to.setName(from.getName());
-        to.setPipelineIdentifier(from.getPipelineIdentifier());
-        to.setStartDate(from.getStartDate());
-        to.setEndDate(from.getEndDate());
-        to.setTimeout(from.getTimeout());
-        to.setResultsLocation(from.getResultsLocation());
+        @Autowired
+        private ShanoirEventService eventService;
 
-        return to;
-    }
+        protected CarminDatasetProcessing updateValues(CarminDatasetProcessing from, CarminDatasetProcessing to) {
+                to.setIdentifier(from.getIdentifier());
+                to.setStatus(from.getStatus());
+                to.setName(from.getName());
+                to.setPipelineIdentifier(from.getPipelineIdentifier());
+                to.setStartDate(from.getStartDate());
+                to.setEndDate(from.getEndDate());
+                to.setTimeout(from.getTimeout());
+                to.setResultsLocation(from.getResultsLocation());
 
-    @Override
-    public CarminDatasetProcessing create(final CarminDatasetProcessing carminDatasetProcessing) {
-        ShanoirEvent event = new ShanoirEvent(ShanoirEventType.IMPORT_DATASET_EVENT,
-                carminDatasetProcessing.getResultsLocation(), KeycloakUtil.getTokenUserId(), "Starting import...",
-                ShanoirEvent.IN_PROGRESS, 0f);
-        eventService.publishEvent(event);
+                return to;
+        }
 
-        CarminDatasetProcessing savedEntity = datasetProcessingRepository.save(carminDatasetProcessing);
+        @Override
+        public CarminDatasetProcessing create(final CarminDatasetProcessing carminDatasetProcessing) {
+                ShanoirEvent event = new ShanoirEvent(ShanoirEventType.IMPORT_DATASET_EVENT,
+                                carminDatasetProcessing.getResultsLocation(), KeycloakUtil.getTokenUserId(),
+                                "Starting import...",
+                                ShanoirEvent.IN_PROGRESS, 0f);
+                eventService.publishEvent(event);
 
-        event.setStatus(ShanoirEvent.SUCCESS);
-        event.setMessage(carminDatasetProcessing.getPipelineIdentifier() + "(" + carminDatasetProcessing.getStudyId() + ")"
-                + ": Successfully created carmin dataset processing ");
-        event.setProgress(1f);
-        eventService.publishEvent(event);
+                CarminDatasetProcessing savedEntity = datasetProcessingRepository.save(carminDatasetProcessing);
 
-        return savedEntity;
-    }
+                event.setStatus(ShanoirEvent.SUCCESS);
+                event.setMessage(carminDatasetProcessing.getPipelineIdentifier() + "("
+                                + carminDatasetProcessing.getStudyId() + ")"
+                                + ": Successfully created carmin dataset processing ");
+                event.setProgress(1f);
+                eventService.publishEvent(event);
 
-    @Override
-    public CarminDatasetProcessing update(final Long datasetProcessingId,
-            final CarminDatasetProcessing carminDatasetProcessing)
-            throws EntityNotFoundException {
-        final Optional<DatasetProcessing> entityDbOpt = datasetProcessingRepository
-                .findById(datasetProcessingId);
-        final DatasetProcessing entityDb = entityDbOpt.orElseThrow(
-                () -> new EntityNotFoundException(carminDatasetProcessing.getClass(), carminDatasetProcessing.getId()));
+                return savedEntity;
+        }
 
-        updateValues(carminDatasetProcessing, (CarminDatasetProcessing) entityDb);
-        return (CarminDatasetProcessing) datasetProcessingRepository.save(entityDb);
+        @Override
+        public CarminDatasetProcessing update(final Long datasetProcessingId,
+                        final CarminDatasetProcessing carminDatasetProcessing)
+                        throws EntityNotFoundException {
+                final Optional<DatasetProcessing> entityDbOpt = datasetProcessingRepository
+                                .findById(datasetProcessingId);
+                final DatasetProcessing entityDb = entityDbOpt.orElseThrow(
+                                () -> new EntityNotFoundException(carminDatasetProcessing.getClass(),
+                                                carminDatasetProcessing.getId()));
 
-    }
+                updateValues(carminDatasetProcessing, (CarminDatasetProcessing) entityDb);
+                return (CarminDatasetProcessing) datasetProcessingRepository.save(entityDb);
 
-    @Override
-    public CarminDatasetProcessing getCarminDatasetProcessingByComment(String comment) throws EntityNotFoundException {
-        return (CarminDatasetProcessing) datasetProcessingRepository.findByComment(comment)
-                .orElseThrow(() -> new EntityNotFoundException("carminDatasetProcessing not found with : " + comment));
-    }
+        }
+
+        @Override
+        public CarminDatasetProcessing getCarminDatasetProcessingByComment(String comment)
+                        throws EntityNotFoundException {
+                return (CarminDatasetProcessing) datasetProcessingRepository.findByComment(comment)
+                                .orElseThrow(() -> new EntityNotFoundException(
+                                                "carminDatasetProcessing not found with : " + comment));
+        }
+
+        @Override
+        public List<CarminDatasetProcessing> getCarminDatasetProcessings() {
+                return Utils.toList(carminDatasetProcessingRepository.findAll());
+        }
 
 }
